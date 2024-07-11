6337fdce142fcaaaeccedd3526afb96e132586cc
Khaled Radwan <khaledradwan96@gmail.com>
2024-07-12 01:03:04 +0300

    add nav (html + js) (toggle icon + hidden nav left + make nav-tab li go up

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ $(document).ready(function(){
         $('body').css('overflow', 'auto')
     })
 })
+
+
+// ========== navbar left ==========
+let boxWidth = $('nav .nav-tab').outerWidth()
+$('#toggleButton').click(function(){
+    // toggle icon
+    $(this).toggleClass('fa-bars')
+    $(this).toggleClass('fa-x')
+    
+    // make nav hidden left
+    if($('nav').css('left') == '0px'){ // nav is shown
+        $('nav').animate({left: -boxWidth}, 500)
+        // make nav-tab li go up
+        for(let i=0; i<5; i++){
+            $('.nav-tab li').eq(i).animate({top: 300}, 500)
+        }
+    }else{ // nav is hidden
+        $('nav').animate({left: 0}, 500)
+        // make nav-tab li go down
+        for(let i=0; i<5; i++){
+            $('.nav-tab li').eq(i).animate({top: 0}, (i + 5) * 100)
+        }
+    }
+})
+// start view
+$('nav').animate({left: -boxWidth}, 10)
+for(let i=0; i<5; i++){
+    $('.nav-tab li').eq(i).animate({top: 300}, 500)
+}
+
